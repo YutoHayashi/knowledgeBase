46234313390697452a8876e963b6e2db1f328bc4
YutoHayashi <YutoHayashi@users.noreply.github.com>
2021-10-29 00:55:36 +0000

    deploy: 8d69e450996525b299bc765a69a84de8146742ba

--- component---src-pages-404-tsx-f873ca8fac01aba703f3.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{506:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(5167),u=n(3786),a=n(7294),l=function(e){return function(e){if(null==e)throw new TypeError("Cannot destructure undefined")}(e),a.createElement(a.Fragment,null,a.createElement(r.p,{title:"404 NotFound"}),a.createElement(u.X,null,"404 NotFound"))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-f873ca8fac01aba703f3.js.map
